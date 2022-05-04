1c78dad39924be9cf11ef990a2664262e5a7e63a
Oana <wannawdo@gmail.com>
2022-05-04 20:08:20 +0300

    seminar 1

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors=require('cors');
+const messagesRouter = require("./router/router"); //look here
+const bodyParser = require("body-parser");
+
+const router = require("./router/router");
+
 const app = express();
-app.use(cors())
+
+app.use(cors());
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/messages",router);
 
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
 app.listen(port, () => {
-  console.log(`Cloud Computing app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
